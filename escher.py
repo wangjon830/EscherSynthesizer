8cea4e052606956ddc1ae460b18cf0167d440a5b
root <root@DESKTOP-1HUIQEL.localdomain>
2021-12-05 23:25:59 -0500

    some funcs

--- escher.py
+++ escher.py
@@ -1,1 +1,175 @@
 
+
+"""
+
+
+
+"""
+
+
+class GoalGraph():
+    def __init__(self):
+        self.G = {self.root}
+        self.R = {}
+        self.E = {}
+        self.root = []
+
+def testProgram(syn, inputoutputs):
+    for program in syn:
+        count = 0
+        for i, inputoutput in enumerate(inputoutputs):
+            out = program.interpret(inputoutput)
+            print (
+                f'{i + 1}. Evaluating programs {program}\non inputoutput examples {inputoutput}. The output of the program is {out}\n')
+            if out == inputoutput["_out"]:
+                count += 1
+            else:
+                break
+        if len(inputoutputs) == count:
+            return program
+    return None
+
+def forward(syn, ops):
+    newSyn = []
+    """for op in ops:
+        if op == PLUS or op == TIMES:
+            for p in syn:
+                typep = p.type
+                if typep == ITE or typep == TIMES or typep == PLUS or typep == VAR:
+                    for term in syn:
+                        type2 = term.type
+                        if type2 != NOT and type2 != AND and type2 != FALSE_exp and type2 !=LT:
+                            if op == PLUS:
+                                newSyn.append(Plus(p, term))
+                            else:
+                                newSyn.append(Times(p, term))
+
+        elif op == ITE:
+            for p in syn:
+                typep = p.type
+                if typep == AND or typep == NOT or typep == LT:
+                    for term in syn:
+                        type2 = term.type
+                        if type2 != NOT and type2 != AND and type2 != FALSE_exp and type2 != LT:
+                            for term2 in syn:
+                                type3 = term2.type
+                                if type3 != NOT and type3 != AND and type3 != FALSE_exp and type3 != LT:
+                                    newSyn.append(Ite(p, term, term2))
+        
+        elif op == AND:
+            for p in syn:
+                typep = p.type
+                if typep == AND or typep == NOT or typep == LT or typep == FALSE_exp:
+                    for term in syn:
+                        type2 = term.type
+                        if type2 == AND or type2 == NOT or type2 == LT or type2 == FALSE_exp:
+                            newSyn.append(And(p, term))
+
+        elif op == NOT:
+            for p in syn:
+                typep = p.type
+                if typep == AND or typep == NOT or typep == LT or typep == FALSE_exp:
+                    newSyn.append(Not(p))
+
+        elif op == LT:
+            for p in syn:
+                typep = p.type
+                if typep == ITE or typep == TIMES or typep == PLUS or typep == VAR or typep == NUM:
+                    for term in syn:
+                        type2 = term.type
+                        if type2 != NOT and type2 != AND and type2 != FALSE_exp and type2 !=LT:
+                            newSyn.append(Lt(p, term) )
+        
+        elif bool == LENGTH:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Length(p))
+        
+        elif bool == REVERSE:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Reverse(p))
+         
+         elif bool == SORT:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Sort(p))
+        
+        elif bool == LSHIFT:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Lshift(p))
+
+        elif bool == RSHIFT:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Rshift(p))
+
+        elif bool == INCREMENT:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Increment(p))
+                    
+        elif bool == MULTIPLYLIST:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(MultiplyList(p))
+
+        elif bool == MAX:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Max(p))
+                    
+        elif bool == MIN:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Min(p))
+                    
+
+        elif bool == HEAD:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Head(p))
+
+
+        elif bool == TAIL:
+            for p in syn:
+                typep = p.type
+                if typep == LIST or typep == SORT or typep == LSHIFT or typep == RSHIFT or typep == INCREMENT or typep == MULTIPLYLIST:
+                    newSyn.append(Tail(p))
+
+        check op and synthesize programs together
+    """
+    return newSyn
+
+def splitgoal():
+    return
+
+def resolve():
+    return
+
+def saturate():
+    return
+
+def escher(syn, goalGraph, ops, inputoutputs, iterations):
+    for inputoutput in inputoutputs:
+        syn.append(inputoutput[0])
+        goalGraph.root.append(inputoutput[1])
+
+    iter = 1
+
+    while iter < iterations:
+        syn = forward(syn, ops)
+        prog = testProgram(syn, inputoutputs)
+        if prog is not None:
+            return prog
